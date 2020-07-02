35ae2c0fa0c2478f073c4585d591029a276840b0
HelloWorld <robpiw@op.pl>
2020-07-02 11:29:06 +0200

    Oddaje zadanie 22.1

--- src/User.java
+++ src/User.java
@@ -1,2 +1,11 @@
 public class User {
+    String firstName;
+    String lastName;
+    int age;
+
+    public User(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
 }
